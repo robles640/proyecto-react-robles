00b67d005abf94473f8303fb12564636ff21062d
Alan Felipe Robles Diaz <roblesdiazalan@gmail.com>
2026-02-06 18:40:54 -0400

    Corregida ruta base en vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diplomado-proyecto-react/'
+  base: '/proyecto-react-robles/' // Cambiado para que coincida con tu repo
 })
